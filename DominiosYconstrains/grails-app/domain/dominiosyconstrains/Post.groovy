36955d3fd43b4778b4dec9b414ffb73361d51c7a
Neill <neillro24124@gmail.com>
2015-04-15 20:35:35 -0500

    Domain Forum and Post

---
+++ DominiosYconstrains/grails-app/domain/dominiosyconstrains/Post.groovy
@@ -0,0 +1,29 @@
+package dominiosyconstrains
+
+class Post {
+
+    String topic
+    Date dateCreated
+    Date lastUpdated
+    boolean itsAllowed
+
+    static constraints = {
+        topic(blank:false, size: 3..50)
+        dateCreated(blank:false, min: new Date()+1)
+        lastUpdated(blank:false, min: new Date()+1)
+        itsAllowed(blank:false)
+
+    }
+
+    public void comment(){
+
+    }
+
+    public void rate(){
+
+    }
+
+    public void share(){
+
+    }
+}
