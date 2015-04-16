d1dcdbd6fc7e56e54110eb62ded878bceab4e02f
jesanchezcr <jesanchezcr@unal.edu.co>
2015-04-15 22:02:23 -0500

    Modified Regular

--- DominiosYconstrains/grails-app/domain/dominiosyconstrains/Regular.groovy
+++ DominiosYconstrains/grails-app/domain/dominiosyconstrains/Regular.groovy
@@ -5,11 +5,13 @@ class Regular extends User{
     int strikesNumber
     int starsNumber
     static hasMany = [pos:Post]
-    static belongsTo = Post
 
     static constraints = {
         postViews blank:false, min: 0
         strikesNumber blank:false, min:0, max:3
         starsNumber blank:false, min:0, max:5
+    }
+    static mapping = {
+        postViews DefaultValue = 0
     }
 }
