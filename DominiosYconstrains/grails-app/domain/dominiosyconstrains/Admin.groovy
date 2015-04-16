0df27e93a3261fcb585a39a503836e0389843d99
jesanchezcr <jesanchezcr@unal.edu.co>
2015-04-15 22:05:22 -0500

    Modified Admin

--- DominiosYconstrains/grails-app/domain/dominiosyconstrains/Admin.groovy
+++ DominiosYconstrains/grails-app/domain/dominiosyconstrains/Admin.groovy
@@ -3,6 +3,7 @@ package dominiosyconstrains
 class Admin extends User{
     int level
     double rating
+    static hasMany = [fo = Forum]
 
     static constraints = {
         level blank:false, size: 1..5
