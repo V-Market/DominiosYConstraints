d5de318f3a474f012b123bb21fc8b17f1755c0c0
jesanchezcr <jesanchezcr@unal.edu.co>
2015-04-15 22:02:14 -0500

    Modified User

--- DominiosYconstrains/grails-app/domain/dominiosyconstrains/User.groovy
+++ DominiosYconstrains/grails-app/domain/dominiosyconstrains/User.groovy
@@ -1,6 +1,5 @@
 package dominiosyconstrains
 
-import org.apache.commons.io.filefilter.FalseFileFilter
 
 class User {
     String name
@@ -9,16 +8,18 @@ class User {
     String username
     String password
 
+
     static constraints = {
         name size: 3..50, blank: false, unique: true, nullable: false
         lastname size: 3..50,blank: false, nullable: false
-        age blank:false, nullable: false
+        age blank:false, nullable: false, min:14
         username blank:false, nullable: false
         password  minSize: 8 , blank: false, nullable: false, validator: {
-            def num = true
-            def myString = password.split()
+            num = true
+            myString = password.split()
             if (myString.count {it == it.toLowerCase()} < 1) num = false
             if (myString.count {it == it.toUpperCase()} < 1) num = false
+            if (myString.count {isInteger(it)}<1) num = false
             if (!num) return false
         }
     }
